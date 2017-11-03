44e6bc780e3db1e4178ea76f3d859b5e008f4c2d
ScottRudiger <scottrudiger@gmail.com>
2017-11-02 20:20:09 -0700

    (refactor) move away from class syntax to avoid neccessity for  keyword

--- AsyncAF.js
+++ AsyncAF.js
@@ -1,16 +1,33 @@
-class AsyncAF {
-  constructor(promises) {
-    this.promise = Promise.resolve(promises);
-  }
-  then(resolve, reject) {
+// const mapAF = require('./mapAF');
+
+// class AsyncAF {
+//   constructor(promises) {
+//     this.promise = Promise.resolve(promises);
+//   }
+//   then(resolve, reject) {
+//     return new AsyncAF(this.promise.then(resolve, reject));
+//   }
+//   catch(reject) {
+//     return this.then(null, reject);
+//   }
+//   mapAF(fn) {
+//     return this.then(array => Promise.all(array.map(fn)));
+//   }
+// }
+
+const AsyncAF = function AsyncAF(promises) {
+  if (!(this instanceof AsyncAF)) return new AsyncAF(promises);
+  this.promise = Promise.resolve(promises);
+  this.then = function thenAF(resolve, reject) {
     return new AsyncAF(this.promise.then(resolve, reject));
-  }
-  catch(reject) {
+  };
+  this.catch = function catchAF(reject) {
     return this.then(null, reject);
-  }
-  mapAF(fn) {
+  };
+  this.mapAF = function mapAF(fn) {
     return this.then(array => Promise.all(array.map(fn)));
-  }
-}
+  };
+  return this;
+};
 
 module.exports = AsyncAF;
