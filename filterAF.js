91418d1e675d7320c2e0349d718a9bdfeab00fc9
ScottRudiger <scottrudiger@gmail.com>
2017-11-06 18:18:42 -0800

    (bugfix) fix bug in naive filterAF implementation

--- filterAF.js
+++ filterAF.js
@@ -1,6 +1,6 @@
 const filterAF = function filterAF(fn) {
-  return this.then(array => Promise.all([array, array.mapAsync(fn)])
-    .then(([arr, mapped]) => arr.filter((v, i) => !!mapped[i])));
+  return Promise.all([this.mapAF(el => el()), this.mapAF(fn)])
+    .then(([values, booleans]) => values.filter((_, i) => !!booleans[i]));
 };
 
 module.exports = filterAF;
