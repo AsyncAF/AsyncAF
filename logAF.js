8299128a8dc329f62c3845c2d15deba106716f2e
ScottRudiger <scottrudiger@gmail.com>
2017-11-17 23:30:36 -0800

    (feat) add line number to logAF

--- logAF.js
+++ logAF.js
@@ -1,4 +1,8 @@
 const logAF = function logAF(...args) {
+  const error = new Error();
+  const start = error.stack.lastIndexOf`/` + 1;
+  const end = error.stack.indexOf(')', start);
+  if (error.stack) args.unshift(`@${error.stack.slice(start, end)}:`);
   Promise.all(args).then(toLog => console.log(...toLog));
 };
 
