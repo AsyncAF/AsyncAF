874d98dcf0a2bf937ac77e7611b18458e986be33
ScottRudiger <scottrudiger@gmail.com>
2017-11-17 23:38:55 -0800

    (reafactor) small refactor of line location in logAF

--- logAF.js
+++ logAF.js
@@ -1,8 +1,10 @@
 const logAF = function logAF(...args) {
   const error = new Error();
-  const start = error.stack.lastIndexOf`/` + 1;
-  const end = error.stack.indexOf(')', start);
-  if (error.stack) args.unshift(`@${error.stack.slice(start, end)}:`);
+  if (error.stack) {
+    const start = error.stack.lastIndexOf`/` + 1;
+    const end = error.stack.indexOf(')', start);
+    args.unshift(`@${error.stack.slice(start, end)}:`);
+  }
   Promise.all(args).then(toLog => console.log(...toLog));
 };
 
