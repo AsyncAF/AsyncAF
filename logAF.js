4aa735c6199693d5afde3b0b7f2e315ea96b16e5
ScottRudiger <scottrudiger@gmail.com>
2017-11-20 09:00:03 -0800

    (refactor) edit logAF labelFormat options to specify inclusion of newline depending on selection

--- logAF.js
+++ logAF.js
@@ -1,3 +1,27 @@
+/*
+* labelFormat options:
+*
+* - fileName -
+* @fileName.js:line:col: non-object
+* @fileName.js:line:col:
+* [Object]
+*
+* - filePath -
+* @/Path/to/current/directory/fileName.js:line:col:
+* non-object
+* @/Path/to/current/directory/fileName.js:line:col:
+* [Object]
+*
+* - parent -
+* @parentDirectory/fileName.js:line:col: non-object
+* @parentDirectory/fileName.js:line:col:
+* [Object]
+*
+* - fatArrow -
+* ========================> non-object
+* ========================> [Object]
+*/
+
 const logAF = function logAF(...args) {
   const error = new Error();
   if (logAF.label && error.stack) {
@@ -6,26 +30,27 @@ const logAF = function logAF(...args) {
       fileName() {
         const start = error.stack.lastIndexOf`/` + 1;
         const end = error.stack.indexOf(')', start);
-        lineNum = `@${error.stack.slice(start, end)}:`;
+        const newLineForObjs = typeof args[0] === 'object' ? '\n' : '';
+        lineNum = `@${error.stack.slice(start, end)}:${newLineForObjs}`;
       },
       filePath() {
         const target = error.stack.lastIndexOf`/`;
         const cutoff = error.stack.slice(0, error.stack.indexOf(')', target));
-        const newLineForPaths = typeof args[0] !== 'object' ? '\n' : '';
-        lineNum = `@${cutoff.slice(cutoff.lastIndexOf`(` + 1)}:${newLineForPaths}`;
+        lineNum = `@${cutoff.slice(cutoff.lastIndexOf`(` + 1)}:\n`;
       },
       parent() {
         const target = error.stack.lastIndexOf`/`;
         const end = error.stack.indexOf(')', target);
         const start = error.stack.slice(0, target).lastIndexOf`/` + 1;
-        lineNum = `@${error.stack.slice(start, end)}:`;
+        const newLineForObjs = typeof args[0] === 'object' ? '\n' : '';
+        lineNum = `@${error.stack.slice(start, end)}:${newLineForObjs}`;
       },
       fatArrow() {
         lineNum = '========================>';
       },
     };
     setFormat[logAF.labelFormat]();
-    args.unshift(typeof args[0] === 'object' ? `${lineNum}\n` : lineNum);
+    args.unshift(lineNum);
   }
   Promise.all(args).then(toLog => console.log(...toLog));
 };
