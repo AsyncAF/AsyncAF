3de6e0827e2cb8167888b263eb549cb6e16d5289
ScottRudiger <scottrudiger@gmail.com>
2017-11-20 06:26:53 -0800

    (feat) add options to logAF & add label option to show or hide line number

--- logAF.js
+++ logAF.js
@@ -1,11 +1,18 @@
 const logAF = function logAF(...args) {
   const error = new Error();
-  if (error.stack) {
+  if (logAF.label && error.stack) {
     const start = error.stack.lastIndexOf`/` + 1;
     const end = error.stack.indexOf(')', start);
-    args.unshift(`@${error.stack.slice(start, end)}:`);
+    const lineNum = `@${error.stack.slice(start, end)}:`;
+    args.unshift(typeof args[0] === 'object' ? `${lineNum}\n` : lineNum);
   }
   Promise.all(args).then(toLog => console.log(...toLog));
+};
+
+logAF.label = true;
+
+logAF.options = function logAFOptions(options) {
+  if (options.label === false) logAF.label = false;
 };
 
 module.exports = logAF;
