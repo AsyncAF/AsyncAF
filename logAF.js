9aeb9fd75042aea12959863607327a1b87c02acf
ScottRudiger <scottrudiger@gmail.com>
2017-12-01 21:51:12 -0800

    (docs) add jsDocs for logAF.options
    (bugfix) fix bug where escaped characters were showing when setting label to false

--- logAF.js
+++ logAF.js
@@ -19,6 +19,8 @@ const logAF = function logAF(...items) {
     Promise.resolve(items[0]).then(item => firstItem = item);
     const lineNum = logAF.setFormat(firstItem);
     items.unshift(lineNum);
+  } else {
+    items.unshift('');
   }
   Promise.all(items).then((toLog) => {
     if (logAF.duration) {
@@ -104,6 +106,111 @@ logAF.setFormat = function setFormat(firstArg) {
   }
   return formats[logAF.labelFormat]();
 };
+
+/**
+ * Sets logging options for the logAF method;
+ * accepts an options Object with the following optional properties:
+ * - label (Boolean) - set to false to disable logging the location of calls to logAF
+ * - duration (Boolean) - set to false to disable logging the time it takes (in secs) to complete each call to logAF
+ * - labelFormat (String) - alters the format of logAF labels; choose between file (default), path, parent, arrow, or custom
+ *
+ * **For example:**
+ * ```javascript
+ * const {logAF} = require('async-af');
+ *
+ * const promise = () => new Promise(resolve => setTimeout(() => resolve(1), 1000));
+ * ```
+ * **default logging**
+ * ```javascript
+ * logAF(promise(), 2);
+ * // @filename.js:212:9: 1 2
+ * // in 0.998 secs
+ * ```
+ * **turn off label**
+ * ```javascript
+ * logAF.options({ label: false });
+ * logAF(promise(), 2);
+ * //  1 2
+ * // in 0.999 secs
+ * ```
+ * **turn off duration**
+ * ```javascript
+ * logAF.options({ duration: false });
+ * logAF(promise(), 2);
+ * // @filename.js:212:9: 1 2
+ * ```
+ * **change labelFormat**
+ * file (default)
+ * ```javascript
+ * logAF.options({ labelFormat: file });
+ * logAF(promise(), 2);
+ * // @filename.js:212:9: 1 2
+ * // in 0.998 secs
+ * ```
+ * path
+ * ```javascript
+ * logAF.options({ labelFormat: path });
+ * logAF(promise(), 2);
+ * // @/Path/to/current/directory/filename.js:212:9:
+ * // 1 2
+ * // in 0.997 secs
+ * ```
+ * parent
+ * ```javascript
+ * logAF.options({ labelFormat: parent });
+ * logAF(promise(), 2);
+ * // @parentDirectory/filename.js:213:9: 1 2
+ * // in 0.998 secs
+ * ```
+ * arrow
+ * ```javascript
+ * logAF.options({ labelFormat: arrow });
+ * logAF(promise(), 2);
+ * // ========================> 1 2
+ * // in 0.999 secs
+ * ```
+ * custom (create your own labelFormat)
+ * - to set a custom labelFormat, first set it to a String starting with `'custom='`
+ * - after `custom=`, insert your desired format as a String
+ * (a String within a String)
+ *
+ * ```javascript
+ * logAF.options({ labelFormat: 'custom="I logged this:"' });
+ * logAF(promise(), 2);
+ * // I logged this: 1 2
+ * // in 1.000 secs
+ * ```
+ *
+ * - your custom format can also access location information as variables, including
+ * `file`, `path`, `parent`, and `arrow`
+ *
+ * e.g., to set the labelFormat to `file:line:col =>` you can use template literals
+ * ```javascript
+ * logAF.options({ labelFormat: 'custom=`${file}:${line}:${col} =>`' });
+ * logAF(promise(), 2);
+ * // filename.js:212:9 => 1 2
+ * // in 0.998 secs
+ * ```
+ *
+ * and just to demonstrate all the location variables in one custom format
+ * ```javascript
+ * logAF.options({
+ *   labelFormat: 'custom=`${arrow}\nline: ${line}\ncol: ${col}\nparent: ${parent}\nfile: ${file}\npath: ${path}`'
+ * });
+ * // ========================>
+ * // line: 212
+ * // col: 9
+ * // parent: parentDirectory/
+ * // file: filename.js
+ * // path: /Path/to/current/directory/ 1 2
+ * // in 0.998 secs
+ * ```
+ *
+ * @param {Object} options - the options for logAF
+ * @param {Boolean} [options.label]
+ * @param {Boolean} [options.duration=true]
+ * @param {string} [options.labelFormat=file]
+ */
 
 logAF.options = function logAFOptions(options) {
   if (options.label === false) logAF.label = false;
