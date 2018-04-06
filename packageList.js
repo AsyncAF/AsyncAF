69b59edb72d234a7bf7b669be0b67b2fb4073c4b
ScottRudiger <scottrudiger@gmail.com>
2018-04-06 03:46:19 -0700

    (feat) add more methods to packageList

--- packageList.js
+++ packageList.js
@@ -1,5 +1,8 @@
 module.exports = {
-  'async-af': './lib/AsyncAF.js',
+  'async-af': './lib/AsyncAF',
+  'async-af.wrapper': './lib/AsyncAfWrapper',
+  'async-af.log': './lib/methods/other/logAF',
   // 'async-af/arrays': 'AsyncAF.arrays.js',
-  'async-af.map': './lib/methods/combined/mapAF.js',
+  'async-af.map': './lib/methods/combined/mapAF',
+  'async-af.forEach': './lib/methods/combined/forEachAF',
 };
