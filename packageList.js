e9f1bec140d5fd9818c85490e48efbbec0626ad7
ScottRudiger <scottrudiger@gmail.com>
2018-04-08 19:46:13 -0700

    (feat) add filterAF to packageList

--- packageList.js
+++ packageList.js
@@ -5,4 +5,5 @@ module.exports = {
   // 'async-af/arrays': 'AsyncAF.arrays.js',
   'async-af.map': './lib/methods/combined/mapAF',
   'async-af.forEach': './lib/methods/combined/forEachAF',
+  'async-af.filter': './lib/methods/combined/filterAF',
 };
