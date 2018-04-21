931c1b22c6ffb5ccc8f11371495d51908a6bb5e7
ScottRudiger <scottrudiger@gmail.com>
2018-04-21 06:48:22 -0700

    fix: use [filebase] instead of [file] in banner so parent dir doesn't show up after refactor

--- webpack.parts.js
+++ webpack.parts.js
@@ -69,7 +69,7 @@ const minify = () => ({
 });
 
 const banner = new BannerPlugin({
-  banner: `${libName} ${description}\n\n[file] v${version}\n\nCopyright (c) 2017-present, ${author}\n\nThis source code is licensed under the ${license} license found in the\nLICENSE file in the root directory of this source tree.`,
+  banner: `${libName} ${description}\n\n[filebase] v${version}\n\nCopyright (c) 2017-present, ${author}\n\nThis source code is licensed under the ${license} license found in the\nLICENSE file in the root directory of this source tree.`,
   entryOnly: true,
 });
 
