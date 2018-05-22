b7fbf434e0a274e8dfddfbc8506d6b4df55d1a78
ScottRudiger <scottrudiger@gmail.com>
2018-05-22 06:41:19 -0700

    feat(concat): implement concatAF method

--- packageList.js
+++ packageList.js
@@ -48,6 +48,7 @@ import findIndexAF from './lib/methods/arrays/findIndexAF';
 import indexOfAF from './lib/methods/arrays/indexOfAF';
 import lastIndexOfAF from './lib/methods/arrays/lastIndexOfAF';
 import joinAF from './lib/methods/arrays/joinAF';
+import concatAF from './lib/methods/arrays/concatAF';
 
 const arrayMethods = [
   name(mapAF, 'mapAF'),
@@ -62,6 +63,7 @@ const arrayMethods = [
   name(indexOfAF, 'indexOfAF'),
   name(lastIndexOfAF, 'lastIndexOfAF'),
   name(joinAF, 'joinAF'),
+  name(concatAF, 'concatAF'),
 ].map(method => [
   method,
   `${libPath}methods/arrays/${method.name}`,
@@ -72,7 +74,11 @@ const arrayMethods = [
 import splitAF from './lib/methods/strings/splitAF';
 
 const stringMethods = [
+  name(includesAF, 'includesAF'),
+  name(indexOfAF, 'indexOfAF'),
+  name(lastIndexOfAF, 'lastIndexOfAF'),
   name(splitAF, 'splitAF'),
+  name(concatAF, 'concatAF'),
 ].map(method => [
   method,
   `${libPath}methods/strings/${method.name}`,
