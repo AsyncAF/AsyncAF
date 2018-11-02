7e0830d7f2cf696031d2fb7cb342d417f0c71b22
ScottRudiger <scottrudiger@gmail.com>
2018-11-01 17:56:14 -0700

    feat(includes): implement series/io.includesAF

--- test/methods/arrays/includesAF.test.js
+++ test/methods/arrays/includesAF.test.js
@@ -64,12 +64,36 @@ describe('includesAF method', () => {
   });
 
   it('should treat holes in sparse arrays as undefined', async () => {
+    /* eslint-disable array-bracket-spacing */
     expect([, , 1].includes(undefined)).to.be.true;
     expect(await AsyncAF([, , 1]).includesAF(undefined)).to.be.true;
+    expect([, , , ].includes(undefined)).to.be.true;
+    expect(await AsyncAF([, , , ]).includesAF(undefined)).to.be.true;
+  }); /* eslint-enable */
+
+  it('should test if an array contains NaN', async () => {
+    expect([, , NaN].includes(NaN)).to.be.true;
+    expect(await AsyncAF([, , NaN]).includesAF(NaN)).to.be.true;
+  });
+
+  it('should default fromIndex to 0 when undefined or invalid', async () => {
+    expect(await AsyncAF([1, 2, 3]).includesAF(1, undefined)).to.be.true;
+    expect(AsyncAF([1, 2, 3]).includesAF(1, Math)).to.not.eventually.throw;
+    expect(await AsyncAF([1, 2, 3]).includesAF(1, Math)).to.be.true;
+  });
+
+  it('should always resolve to false given a fromIndex of an array\'s length', async () => {
+    expect([1, 2, 3].includes(3, 3)).to.be.false;
+    expect(await AsyncAF([1, 2, 3]).includesAF(3, 3)).to.be.false;
+  });
+
+  it('should resolve to false given an empty array', async () => {
+    expect([].includes(undefined)).to.be.false;
+    expect(await AsyncAF([]).includesAF(undefined)).to.be.false;
   });
 
   context('should work on an array of promises', () => {
-    const nums = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(4)];
+    const nums = [1, 2, 4].map(n => Promise.resolve(n));
     it('and resolve to true if the array includes the specified element', async () => {
       expect(await AsyncAF(nums).includesAF(1)).to.be.true;
     });
